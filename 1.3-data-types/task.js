1d90d67ea67472e6a812be152cf1c2c7428af5e5
Maxim <maxime.axenoff@yandex.ru>
2021-04-07 22:00:41 +0500

    v1.3

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,52 @@
+"use strict";
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    if (Number.isInteger(+percent)) {
+        console.log(percent);
+    } else {
+        return (`Параметр процентая ставка содержит неправильное значение ${percent}`);
+    }
+
+    if (Number.isInteger(+contribution)) {
+        console.log(contribution);
+    } else {
+        return (`Параметр сумма первоначального взноса содержит неправильное значение ${contribution}`);
+    }
+
+    if (Number.isInteger(+amount)) {
+        console.log(amount);
+    } else {
+        return (`Параметр сумма кредита содержит неправильное значение ${amount}`);
+    }
+
+    if (Number.isInteger(+date)) {
+        console.log(date);
+    } else {
+        return (`Параметр срок кредита содержит неправильное значение ${date}`);
+    }
+
+    let dateYear = date.getFullYear() - new Date().getFullYear();
+    let month;
+
+    if (dateYear === 0) {
+        month = Math.abs(date.getMonth() - new Date().getMonth());
+    } else {
+        month = Math.abs(date.getMonth() - new Date().getMonth()) + (12 * dateYear);
+    }
+
+    let bodyCredit = +amount - +contribution;
+    let percentMonth = (+percent / 12) / 100;
+    let amout = bodyCredit * (percentMonth + percentMonth / (((1 + percentMonth) ** month) - 1));
+    let totalAmount = amout * month;
+
+    return totalAmount.toFixed(2);
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    let greeting;
+    if (name) {
+        greeting = `Привет, мир! Меня зовут ${name}.`;
+    } else {
+        greeting = `Привет, мир! Меня зовут Аноним.`;
+    }
+    return greeting;
 }
