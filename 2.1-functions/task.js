879e8bbf5ddecfc100b669c54f88b7f162c316f6
Maxim <maxime.axenoff@yandex.ru>
2021-04-24 12:18:33 +0500

    2.1-functions v2.1

--- 2.1-functions/task.js
+++ 2.1-functions/task.js
@@ -1,67 +1,67 @@
 "use strict";
 
-// console.log(`Задание 1`);
+console.log(`Задание 1`);
 
-// function getSolutions(a, b, c) {
-//     let d = (b ** 2) - (4 * a * c);
+function getSolutions(a, b, c) {
+    let d = (b ** 2) - (4 * a * c);
 
-//     if (d < 0) {
-//         return { D: d, roots: [] };
-//     } else if (d === 0) {
-//         let x1 = -b / (2 * a);
-//         return { D: d, roots: [x1] };
-//     } else {
-//         let x1 = (-b + Math.sqrt(d)) / (2 * a);
-//         let x2 = (-b - Math.sqrt(d)) / (2 * a);
-//         console.log(x1, x2);
-//         return { D: d, roots: [x1, x2] };
-//     }
+    if (d < 0) {
+        return { D: d, roots: [] };
+    } else if (d === 0) {
+        let x1 = -b / (2 * a);
+        return { D: d, roots: [x1] };
+    } else {
+        let x1 = (-b + Math.sqrt(d)) / (2 * a);
+        let x2 = (-b - Math.sqrt(d)) / (2 * a);
+        console.log(x1, x2);
+        return { D: d, roots: [x1, x2] };
+    }
 
-// }
+}
 
-// function showSolutionsMessage(a, b, c) {
-//     let result = getSolutions(a, b, c);
-//     console.log(`Вычисляем корни квадратного уровнения ${a}x² + ${b}x + ${c}`);
-//     console.log(`Значение дискриминанта: ${result.D}`);
+function showSolutionsMessage(a, b, c) {
+    let result = getSolutions(a, b, c);
+    console.log(`Вычисляем корни квадратного уровнения ${a}x² + ${b}x + ${c}`);
+    console.log(`Значение дискриминанта: ${result.D}`);
 
-//     if (result.roots.length === 0) {
-//         console.log(`Уравнение не имеет вещественных корней`);
-//     } else if (result.roots.length === 1) {
-//         console.log(`Уравнение имеет один корень X₁ = ${result.roots[0]}`);
-//     } else {
-//         console.log(`Уравнение имеет два корня. X₁ = ${result.roots[0]}, X₂ = ${result.roots[1]}`);
-//     }
-// }
+    if (result.roots.length === 0) {
+        console.log(`Уравнение не имеет вещественных корней`);
+    } else if (result.roots.length === 1) {
+        console.log(`Уравнение имеет один корень X₁ = ${result.roots[0]}`);
+    } else {
+        console.log(`Уравнение имеет два корня. X₁ = ${result.roots[0]}, X₂ = ${result.roots[1]}`);
+    }
+}
 
-// showSolutionsMessage(2, 4, 2);
+showSolutionsMessage(2, 4, 2);
 
-// console.log(`Задание 2`);
+console.log(`Задание 2`);
 
-// function getAverageScore(data) {
-//     let avarageMark = {};
-//     for (let prop in data) {
-//         let avarageValue = data[prop].reduce((acc, number) => (acc + number)) / data[prop].length;
-//         avarageMark[prop] = avarageValue;
-//     } return avarageMark;
-// };
+function getAverageScore(data) {
+    let avarageMark = {};
+    for (let prop in data) {
+        let avarageValue = data[prop].reduce((acc, number) => (acc + number)) / data[prop].length;
+        avarageMark[prop] = avarageValue;
+    } return avarageMark;
+};
 
-// function getAverageMark(marks) {
-//     let answerMarks = marks;
-//     let sum = 0;
-//     for (let prop in marks) {
-//         sum = (marks[prop] + sum);
-//     }
-//     if (Object.keys(marks).length > 0) {
-//         answerMarks.avarage = sum / Object.keys(marks).length;
-//     } else {
-//         answerMarks.avarage = 0;
-//     }
+function getAverageMark(marks) {
+    let answerMarks = marks;
+    let sum = 0;
+    for (let prop in marks) {
+        sum = (marks[prop] + sum);
+    }
+    if (Object.keys(marks).length > 0) {
+        answerMarks.avarage = sum / Object.keys(marks).length;
+    } else {
+        answerMarks.avarage = 0;
+    }
 
-//     console.log(answerMarks);
-//     return answerMarks;
-// }
+    console.log(answerMarks);
+    return answerMarks;
+}
 
-// getAverageMark(getAverageScore({ algebra: [2, 4, 5, 2, 3, 4], geometry: [2, 4, 5], russian: [3, 3, 4, 5], physics: [5, 5], music: [2, 2, 6], english: [4, 4, 3], poetry: [5, 3, 4], chemistry: [2], french: [4, 4] }));
+getAverageMark(getAverageScore({ algebra: [2, 4, 5, 2, 3, 4], geometry: [2, 4, 5], russian: [3, 3, 4, 5], physics: [5, 5], music: [2, 2, 6], english: [4, 4, 3], poetry: [5, 3, 4], chemistry: [2], french: [4, 4] }));
 
 
 console.log(`Задание 3`);
